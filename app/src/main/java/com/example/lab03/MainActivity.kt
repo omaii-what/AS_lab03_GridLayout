31f21b204342e361b7e662405708b7d0c9b22b17
Anna <alabenko0503@gmail.com>
2026-01-28 17:23:57 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lab03/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.as_lab03_gridlayout
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val newEntryButton: Button = findViewById(R.id.newEntryButton)
+        val historyButton: Button = findViewById(R.id.historyButton)
+        val statsButton: Button = findViewById(R.id.statsButton)
+        val settingsButton: Button = findViewById(R.id.settingsButton)
+
+        newEntryButton.setOnClickListener {
+            Toast.makeText(this, "Создание новой записи", Toast.LENGTH_SHORT).show()
+        }
+        historyButton.setOnClickListener {
+            Toast.makeText(this, "Просмотр истории", Toast.LENGTH_SHORT).show()
+        }
+        statsButton.setOnClickListener {
+            Toast.makeText(this, "Просмотр статистики", Toast.LENGTH_SHORT).show()
+        }
+        settingsButton.setOnClickListener {
+            Toast.makeText(this, "Открытие настроек", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
